671e68a0ca275e6c94c79d059af5278de5c4a138
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-24 11:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+// Adding an event listener to each button
+document.getElementById("buttons").addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn")) {
+        // If a button with class "btn" is clicked, play the corresponding audio
+        playAudio(event.target.innerText.toLowerCase() + ".mp3");
+    } else if (event.target.classList.contains("stop")) {
+        // If the stop button is clicked, stop the audio
+        stopAudio();
+    }
+});
+
+let audio;
+
+function playAudio(filename) {
+    stopAudio(); // Stop any currently playing audio
+    audio = new Audio(`sounds/${filename}`);
+    audio.play();
+}
+
+function stopAudio() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
